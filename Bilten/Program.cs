3c564fdc08b72e4060d5a39f3c101d6bd6ded9b6
Sasa Stankovic <sstankovic617@gmail.com>
2016-12-04 16:35:39 +0100

    Ispravljena putanja do fajla za apdejtovanje baze. Dodato brisanje fajla NHibernateConfig kada se apdejtuje baza

--- Bilten/Program.cs
+++ Bilten/Program.cs
@@ -34,31 +34,47 @@ namespace Bilten
             //dfs.createGraphFromExportSqlCeStript(schemaFile);
             //dfs.doDFS();
 
-            int verzijaBaze = DatabaseUpdater.getDatabaseVersionNumber();
-            if (verzijaBaze != VERZIJA_PROGRAMA)
+            try
             {
-                if (verzijaBaze == 0)
+                int verzijaBaze = DatabaseUpdater.getDatabaseVersionNumber();
+                if (verzijaBaze != VERZIJA_PROGRAMA)
                 {
-                    string msg = "Bazu podataka je nemoguce konvertovati da radi sa trenutnom verzijom programa.";
-                    MessageBox.Show(msg, "Bilten");
-                    return;
-                }
-                if (verzijaBaze > VERZIJA_PROGRAMA)
-                {
-                    string msg = "Greska u programu. Verzija baze je veca od verzije programa.";
-                    MessageBox.Show(msg, "Bilten");
-                    return;
-                }
+                    if (verzijaBaze == 0)
+                    {
+                        string msg = "Bazu podataka je nemoguce konvertovati da radi sa trenutnom verzijom programa.";
+                        MessageBox.Show(msg, "Bilten");
+                        return;
+                    }
+                    if (verzijaBaze > VERZIJA_PROGRAMA)
+                    {
+                        string msg = "Greska u programu. Verzija baze je veca od verzije programa.";
+                        MessageBox.Show(msg, "Bilten");
+                        return;
+                    }
 
-                if (verzijaBaze == 1 && VERZIJA_PROGRAMA > 1)
-                {
-                    string databaseFile = "BiltenPodaci.sdf";
-                    SqlCeUtilities.ExecuteScript(databaseFile, "", Path.GetFullPath(@"..\..\DatabaseUpdate_version2.sql"));
+                    bool converted = false;
+                    if (verzijaBaze == 1 && VERZIJA_PROGRAMA > 1)
+                    {
+                        string databaseFile = "BiltenPodaci.sdf";
+                        // TODO4: Prebaci DatabaseUpdate_version2.sql da bude embedded resource
+                        SqlCeUtilities.ExecuteScript(databaseFile, "", Path.GetFullPath(@"DatabaseUpdate_version2.sql"));
 
-                    verzijaBaze = DatabaseUpdater.getDatabaseVersionNumber();
-                    string msg = String.Format("Baza podataka je konvertovana u verziju {0}.", verzijaBaze);
-                    MessageBox.Show(msg, "Bilten");
+                        verzijaBaze = DatabaseUpdater.getDatabaseVersionNumber();
+                        string msg = String.Format("Baza podataka je konvertovana u verziju {0}.", verzijaBaze);
+                        MessageBox.Show(msg, "Bilten");
+                        converted = true;
+                    }
+
+                    if (converted && File.Exists("NHibernateConfig"))
+                    {
+                        File.Delete("NHibernateConfig");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
             }
 
             Language.SetKeyboardLanguage(Language.acKeyboardLanguage.hklSerbianLatin);
