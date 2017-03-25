898391704cbfe50b1acb291407bf67d18def1ec0
Sasa Stankovic <sstankovic617@gmail.com>
2017-03-25 10:17:51 +0100

    Izbrisani fajlovi ColumnAdder.cs i DatabaseUpdater.cs. Kod prebacen u SqlCeUtilities

--- Bilten/Util/SqlCeUtilities.cs
+++ Bilten/Util/SqlCeUtilities.cs
@@ -121,4 +121,166 @@ namespace Bilten.Dao
             }
         }
     }
+
+    public class ColumnAdder
+    {
+        private string table;
+        private string column;
+
+        private SqlCeCommand addColumnCommand;
+        private SqlCeCommand updateColumnCommand;
+
+        public ColumnAdder(string table, string column, SqlDbType type,
+            Nullable<int> size, object columnValue)
+        {
+            this.table = table;
+            this.column = column;
+
+            string addColumnSql = "ALTER TABLE [" + table + "] ADD COLUMN [" + column + "] " +
+                getTypeSqlString(type, size);
+            string updateColumnSql = "UPDATE [" + table + "] SET [" + column + "] = @" +
+                column;
+
+            addColumnCommand = new SqlCeCommand(addColumnSql);
+            updateColumnCommand = new SqlCeCommand(updateColumnSql);
+            if (size != null)
+                updateColumnCommand.Parameters.Add("@" + column, type, size.Value).Value = columnValue;
+            else
+                updateColumnCommand.Parameters.Add("@" + column, type).Value = columnValue;
+        }
+
+        private string getTypeSqlString(SqlDbType type, Nullable<int> size)
+        {
+            switch (type)
+            {
+                case SqlDbType.NVarChar:
+                    return "nvarchar(" + size.Value + ")";
+
+                case SqlDbType.TinyInt:
+                    return "tinyint";
+
+                default:
+                    throw new Exception();
+            }
+        }
+
+        public void addColumn()
+        {
+            if (!columnExists(table, column))
+            {
+                SqlCeConnection conn = new SqlCeConnection(ConfigurationParameters.ConnectionString);
+                string errMsg = "Neuspesna promena baze.";
+                SqlCeTransaction tr = null;
+                try
+                {
+                    conn.Open();
+                    tr = conn.BeginTransaction();
+
+                    addColumnCommand.Connection = conn;
+                    addColumnCommand.Transaction = tr;
+                    addColumnCommand.ExecuteNonQuery();
+
+                    updateColumnCommand.Connection = conn;
+                    updateColumnCommand.Transaction = tr;
+                    updateColumnCommand.ExecuteNonQuery();
+
+                    tr.Commit();
+                }
+                catch (SqlCeException e)
+                {
+                    // in Open()
+                    if (tr != null)
+                        tr.Rollback(); // TODO: this can throw Exception and InvalidOperationException
+                    throw new InfrastructureException(errMsg, e);
+                }
+                catch (InvalidOperationException e)
+                {
+                    // in ExecuteNonQuery(), ExecureScalar()
+                    if (tr != null)
+                        tr.Rollback();
+                    throw new InfrastructureException(errMsg, e);
+                }
+                // za svaki slucaj
+                catch (Exception)
+                {
+                    if (tr != null)
+                        tr.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private bool columnExists(string table, string column)
+        {
+            string sql = "SELECT * FROM INFORMATION_SCHEMA.COLUMNS " +
+                "WHERE TABLE_NAME = @TableName AND COLUMN_NAME = @ColumnName";
+            SqlCeCommand cmd = new SqlCeCommand(sql);
+            cmd.Parameters.Add("@TableName", SqlDbType.NVarChar).Value = table;
+            cmd.Parameters.Add("@ColumnName", SqlDbType.NVarChar).Value = column;
+
+            string errMsg = "Greska prilikom citanja podataka iz baze.";
+            SqlCeDataReader rdr = SqlCeUtilities.executeReader(cmd, errMsg);
+            bool result = false;
+            if (rdr.Read())
+                result = true;
+            rdr.Close();
+            return result;
+        }
+    }
+
+    public class DatabaseUpdater
+    {
+        public void updateDatabase()
+        {
+            addColumn("Elementi", "NazivPoGimnasticaru", SqlDbType.NVarChar, 64, "");
+            addColumn("Slike", "ProcenatRedukcije", SqlDbType.TinyInt, null, 100);
+        }
+
+        private void addColumn(string table, string column, SqlDbType type, Nullable<int> size, object columnValue)
+        {
+            ColumnAdder adder = new ColumnAdder(table, column, type, size, columnValue);
+            adder.addColumn();
+        }
+
+        private static bool tableExists(string tableName)
+        {
+            string sql = @"SELECT * FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_NAME = @TableName";
+            SqlCeCommand cmd = new SqlCeCommand(sql);
+            cmd.Parameters.Add("@TableName", SqlDbType.NVarChar).Value = tableName;
+
+            string errMsg = "Greska prilikom citanja podataka iz baze.";
+            SqlCeDataReader rdr = SqlCeUtilities.executeReader(cmd, errMsg);
+            bool result = false;
+            if (rdr.Read())
+                result = true;
+            rdr.Close();
+            return result;
+        }
+
+        public static int getDatabaseVersionNumber()
+        {
+            string tableName = "verzija_baze";
+            if (!DatabaseUpdater.tableExists(tableName))
+            {
+                return 0;
+            }
+
+            string sql = "SELECT broj_verzije FROM " + tableName +
+                " WHERE verzija_id = 1";
+            SqlCeCommand cmd = new SqlCeCommand(sql);
+
+            string errMsg = "Greska prilikom citanja podataka iz baze.";
+            SqlCeDataReader rdr = SqlCeUtilities.executeReader(cmd, errMsg);
+            int result = 0;
+            if (rdr.Read())
+                result = rdr.GetInt32(0);
+            rdr.Close();
+            return result;
+        }
+    }
 }
